c2bf53023ff2addaab46d1d55cfb877a1b6fa9df
Jon Wang <jon.wang@wakeasp.com.tw>
2019-11-04 15:53:39 +0800

    ch.22 done

--- tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
@@ -5,6 +5,7 @@ namespace Tests\AppBundle\Service;
 use AppBundle\Entity\Dinosaur;
 use AppBundle\Entity\Enclosure;
 use AppBundle\Entity\Security;
+use AppBundle\Factory\DinosaurFactory;
 use AppBundle\Service\EnclosureBuilderService;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\EntityManager;
@@ -24,8 +25,18 @@ class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
     {
         //self::bootKernel();
         /** @var EnclosureBuilderService $enclosureBuilderService */
-        $enclosureBuilderService = self::$kernel->getContainer()
-            ->get("test.".EnclosureBuilderService::class);
+//        $enclosureBuilderService = self::$kernel->getContainer()
+//            ->get("test.".EnclosureBuilderService::class);
+
+        $dinoFactory = $this->createMock(DinosaurFactory::class);
+        $dinoFactory->expects($this->any())
+            ->method('growFromSpecification')
+            ->willReturnCallback(function($spec) {
+                return new Dinosaur();
+            });
+        $enclosureBuilderService = new EnclosureBuilderService(
+            $this->getEntityManager(),
+            $dinoFactory);
 
 
         $enclosureBuilderService->buildEnclosure();
