d2be09a7d870c900f721f97c30b7f2e8b5fc5c2d
Jon Wang <jon.wang@wakeasp.com.tw>
2019-11-05 15:54:58 +0800

    ch.27 done

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Enclosure;
+use AppBundle\Factory\DinosaurFactory;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,4 +22,33 @@ class DefaultController extends Controller
             "enclosures"=>$enclosures
         ]);
     }
+
+    /**
+     * @Route("/grow", name="grow_dinosaur")
+     * @Method({"POST"})
+     */
+    public function growAction(Request $request, DinosaurFactory $dinosaurFactory)
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        $enclosure = $manager->getRepository(Enclosure::class)
+            ->find($request->request->get('enclosure'));
+
+        $specification = $request->request->get('specification');
+        $dinosaur = $dinosaurFactory->growFromSpecification($specification);
+
+        $dinosaur->setEnclosure($enclosure);
+        $enclosure->addDinosaur($dinosaur);
+
+        $manager->flush();
+
+        $this->addFlash('success', sprintf(
+            'Grew a %s in enclosure #%d',
+            mb_strtolower($specification),
+            $enclosure->getId()
+        ));
+
+        return $this->redirectToRoute('homepage');
+    }
+
 }
